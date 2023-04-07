6d65cf7a54ab877382cdfd961d9ac55aac81610e
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-04-07 21:23:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 342.37 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -15,5 +15,32 @@ class Solution {
         answer = participant[participant.length-1];
         
         return answer;
+        
+//         HashMap 사용
+//         HashMap<String, Integer> map = new HashMap<>();
+
+//         // 참여자 명단을 HashMap에 추가
+//         for (String name : participant) {
+//             if (map.containsKey(name)) {
+//                 map.put(name, map.get(name) + 1);
+//             } else {
+//                 map.put(name, 1);
+//             }
+//         }
+
+//         // 완주자 명단을 HashMap에서 제거
+//         for (String name : completion) {
+//             if (map.get(name) > 1) {
+//                 map.put(name, map.get(name) - 1);
+//             } else {
+//                 map.remove(name);
+//             }
+//         }
+
+//         // HashMap에 남은 이름이 미완주자 이름
+//         for (String name : map.keySet()) {
+//             answer = name;
+//         }
+
     }
 }
