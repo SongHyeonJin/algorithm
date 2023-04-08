137fde8f9f0bdbad0c918f83d3b3ab2b55fcf64d
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-04-08 14:57:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 8.56 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -7,6 +7,12 @@ class Solution {
             n/=3;
         }
         answer = Integer.parseInt(str, 3);
+        
+        String s= Integer.toString(n,3);
+
+        // StringBuilder sb = new StringBuilder(s);
+        // String reverse = sb.reverse().toString();
+        // answer = Integer.parseInt(reverse,3);
         return answer;
     }
 }
