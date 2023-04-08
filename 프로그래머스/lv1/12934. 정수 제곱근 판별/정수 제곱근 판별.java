4cd40dd9ab230e0ba336097dacf6413124c5da1a
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-04-08 10:19:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -8,5 +8,13 @@ class Solution {
             }
         }
         return answer == 0 ? -1 : answer;
+        // long answer = 0;
+        // double sqrt = Math.sqrt(n);
+        // if(sqrt%1!=0){
+        //     answer = -1;
+        // } else {
+        //     answer = (long)(sqrt+1)*(long)(sqrt+1);
+        // }
+        // return answer;
     }
 }
