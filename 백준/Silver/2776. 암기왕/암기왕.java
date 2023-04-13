043334eaf0df94ccc3018e32a3e0cb2b6b635511
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-04-13 21:54:28 +0900

    [Silver IV] Title: 암기왕, Time: 1644 ms, Memory: 286348 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -11,25 +11,28 @@ public class Main {
 
         int testCase = Integer.parseInt(br.readLine());
         int num = 0;
-
+        Set<Integer> set = new HashSet<>();
         for(int i=0; i<testCase; i++){
+            // 수첩 1
             int N = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-            Set<Integer> list = new HashSet<>();
+            
             for(int j=0; j<N; j++){
-                list.add(Integer.parseInt(st.nextToken()));
+                set.add(Integer.parseInt(st.nextToken()));
             }
 
+            // 수첩 2
             int M = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<M; j++){
                 num = Integer.parseInt(st.nextToken());
-                if(list.contains(num)){
+                if(set.contains(num)){
                     sb.append(1).append("\n");
                 }else{
                     sb.append(0).append("\n");
                 }
             }
+            set.clear();
         }
         System.out.println(sb.toString());
     }
