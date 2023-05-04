cd95d661a5326ffe77cf8fdc5c26e48283eee089
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-05-04 16:41:59 +0900

    [Silver IV] Title: 단어 개수 세기, Time: 148 ms, Memory: 14872 KB -BaekjoonHub

--- 백준/Silver/19844. 단어 개수 세기/단어 개수 세기.java
+++ 백준/Silver/19844. 단어 개수 세기/단어 개수 세기.java
@@ -11,12 +11,14 @@ public class Main {
         String sentence = br.readLine();
 
         String[] words = sentence.split("[\\s-]");
-       
-        Pattern pattern =Pattern.compile("^(c|j|n|m|t|s|l|d|qu)(')[aeiouh]");
+        // 주어진 정규표현식으로부터 패턴을 만듭니다.
+        Pattern pattern =Pattern.compile("^(c|j|n|m|t|s|l|d|qu)'[aeiouh]");
         int count = words.length;
 
         for (String word : words){
+            // 대상 문자열과 패턴이 일치할 경우 true 반환합니다.
             Matcher matcher = pattern.matcher(word);
+            // 대상 문자열과 패턴이 일치하는 경우 true 를 반환하고, 그 위치로 이동합니다.
             while (matcher.find()) count++;
         }
         System.out.println(count);
