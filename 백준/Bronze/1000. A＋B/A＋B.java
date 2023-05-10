fcf5483dcd9eb5287d8c8739fab0821dd7cc080d
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-05-11 00:36:48 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
- 
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int A = in.nextInt();
-		int B = in.nextInt();
-		
-		System.out.println(A+B);
- 
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+    }
+
 }
