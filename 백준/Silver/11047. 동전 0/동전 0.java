a32fcaa81d7c9d626008cd1630f12e4c259cae94
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-05-18 17:18:27 +0900

    [Silver IV] Title: 동전 0, Time: 236 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Integer[] money = new Integer[N];
+        for(int i=0; i<N; i++){
+            money[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for(int i=N-1; i>=0; i--){
+            if(money[i] <= K){
+                count += (K/money[i]);
+                K %= money[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
