5e02f2d8d72e724ed3a1d2aac2d2ab2b287b3cf7
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-07-24 21:57:23 +0900

    [Gold IV] Title: 문자열 폭발, Time: 500 ms, Memory: 34468 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -14,21 +14,24 @@ public class Main {
         int bomb_len = bomb.length();
         Stack<Character> stack = new Stack<>();
 
-        for(int i=0; i<string_len; i++){
-            int count = 0;
+        for (int i = 0; i < string_len; i++) {
             stack.push(string.charAt(i));
-            if(stack.size() >= bomb_len){
-                for(int j=0; j<bomb_len; j++){
-                    if(stack.get(stack.size() - bomb_len + j) == bomb.charAt(j)) count++;
-                    if(bomb_len == count){
-                        for(int k=0; k<bomb_len; k++){
-                            stack.pop();
-                        }
+            if (stack.size() >= bomb_len) {
+                boolean flag = true;
+                for (int j = 0; j < bomb_len; j++) {
+                    if (stack.get(stack.size() - bomb_len + j) != bomb.charAt(j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    for (int k = 0; k < bomb_len; k++) {
+                        stack.pop();
                     }
                 }
             }
         }
-        for(char c : stack) sb.append(c);
+        for (char c : stack) sb.append(c);
         System.out.println(stack.isEmpty() ? "FRULA" : sb);
     }
 }
