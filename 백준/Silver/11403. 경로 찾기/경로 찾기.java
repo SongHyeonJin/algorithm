d922649a2654cf8c4904abc00c87ac495d4c85af
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-07-30 01:26:24 +0900

    [Silver I] Title: 경로 찾기, Time: 188 ms, Memory: 15300 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        floyd();
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                sb.append(map[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+    static void floyd(){
+        for(int k=0; k<N; k++){
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    if(map[i][k] == 1 && map[k][j] == 1) map[i][j] = 1;
+                }
+            }
+        }
+    }
+}
