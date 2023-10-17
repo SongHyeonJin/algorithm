215e28eb656e5f8a154c00cf6290a8705525d85f
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-10-17 21:54:26 +0900

    [level unrated] Title: 컨트롤 제트, Time: 0.13 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] strs = s.split(" ");
+        for(int i=0; i<strs.length; i++){
+            if(strs[i].equals("Z")) {
+                answer -= Integer.parseInt(strs[i-1]);
+                continue;
+            }
+            answer += Integer.parseInt(strs[i]);
+        }
+        return answer;
+    }
+}
