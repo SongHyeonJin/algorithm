e6a737bdda1bde91319578943e8d89f70b83306a
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-11-07 22:02:25 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -2,29 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
-        while (T --> 0){
-            HashMap<String, Integer> map = new HashMap<>();
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0; i<t; i++){
+            int n = Integer.parseInt(br.readLine());
+            HashMap<String, Integer> hashMap = new HashMap<>();
 
-            int N = Integer.parseInt(br.readLine());
-            while (N --> 0){
-                String[] type = br.readLine().split(" ");
-                map.put(type[1], map.getOrDefault(type[1], 0)+1);
+            for(int j=0; j<n; j++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                st.nextToken();
+                String type = st.nextToken();
+                hashMap.put(type, hashMap.getOrDefault(type, 0) + 1);
             }
-            Iterator<Integer> iterator = map.values().iterator();
 
-            int result = 1;
-            while (iterator.hasNext()){
-                result *= iterator.next().intValue() + 1;
+            int answer = 1;
+            for(int val : hashMap.values()){
+                answer *= val + 1;
             }
-            sb.append(result-1).append("\n");
+            sb.append(answer-1).append("\n");
         }
         System.out.println(sb);
     }
