1adb0b0c31694643107b79f507e561f422524b6f
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2023-11-16 13:52:15 +0900

    [level unrated] Title: 이어 붙인 수, Time: 4.11 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String strOdd = "";
+        String strEven = "";
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i]%2==0)
+                strEven += String.valueOf(num_list[i]);
+            else
+                strOdd += String.valueOf(num_list[i]);
+        }
+        answer = Integer.parseInt(strEven) + Integer.parseInt(strOdd);
+        return answer;
+    }
+}
