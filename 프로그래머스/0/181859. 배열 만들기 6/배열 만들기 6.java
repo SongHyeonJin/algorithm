745cea6c3060044b11cea84bdeed86a9b72b3a34
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2024-08-11 23:23:45 +0900

    [level 0] Title: 배열 만들기 6, Time: 37.12 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        int i=0;
+        while(i<arr.length){
+            if(answer.size()==0){
+                answer.add(arr[i]);
+                i++;
+            } else{
+                if(answer.get(answer.size()-1)==arr[i]){
+                    answer.remove(answer.size()-1);
+                    i++;
+                } else{
+                    answer.add(arr[i]);
+                    i++;
+                }
+            }
+        }
+        if(answer.isEmpty()) answer.add(-1);
+        return answer.stream().mapToInt(x -> x).toArray();
+    }
+}
