205b70ae9b513600fadc05e59c46641550d570e4
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2024-12-02 17:38:38 +0900

    [Silver III] Title: N과 M (3), Time: 424 ms, Memory: 68564 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -5,33 +5,30 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
-    static boolean[] visited;
     static int[] arr;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[n];
         arr = new int[m];
-        dfs(0);
+        recur(0);
         System.out.println(sb);
     }
-    static void dfs(int depth){
-        if(depth == m){
-            for(int val : arr){
+
+    static void recur(int num){
+        if (num == m){
+            for (int val : arr)
                 sb.append(val).append(" ");
-            }
             sb.append("\n");
             return;
         }
-        for(int i=0; i<n; i++){
-            visited[i] = true;
-            arr[depth] = i+1;
-            dfs(depth+1);
-            visited[i]= false;
+        for (int i=0; i<n; i++){
+            arr[num] = i+1;
+            recur(num+1);
         }
     }
 }
