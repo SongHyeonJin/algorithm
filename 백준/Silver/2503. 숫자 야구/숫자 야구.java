61a62fc06286b7519b4da8fda75aecedba15af31
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2024-12-03 21:33:16 +0900

    [Silver III] Title: 숫자 야구, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -9,11 +9,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         n = Integer.parseInt(br.readLine());
         game = new int[n][3];
-        int count = 0;
+        int answer = 0;
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             game[i][0] = Integer.parseInt(st.nextToken());
             game[i][1] = Integer.parseInt(st.nextToken());
@@ -24,25 +25,24 @@ public class Main {
             for (int j = 1; j <= 9; j++)
                 for (int k = 1; k <= 9; k++)
                     if(i != j && i != k && j != k)
-                        if(sol(i, j, k))
-                            count++;
-        System.out.println(count);
+                        if (sol(i, j, k)) answer++;
+        System.out.println(answer);
     }
-    static boolean sol(int n1, int n2, int n3){
+    static boolean sol(int num1, int num2, int num3){
         int first, second, third, strike, ball;
         for (int i = 0; i < n; i++) {
             strike = 0;
             ball = 0;
             first = game[i][0] / 100;
-            second = (game[i][0]%100) / 10;
-            third =  game[i][0] % 10;
+            second = (game[i][0]/10)%10;
+            third = game[i][0]%10;
 
-            if(first==n1) strike++;
-            else if(first==n2 || first==n3) ball++;
-            if(second==n2) strike++;
-            else if (second==n1 || second==n3) ball++;
-            if(third==n3) strike++;
-            else if (third==n1 || third==n2) ball++;
+            if(first==num1) strike++;
+            else if(first==num2 || first==num3) ball++;
+            if(second==num2) strike++;
+            else if(second==num1 || second==num3) ball++;
+            if(third==num3) strike++;
+            else if(third==num1 || third==num2) ball++;
 
             if(strike != game[i][1] || ball != game[i][2]) return false;
         }
