4f0332cfcff096e93db9d22d9e20d6485f93a0c4
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2024-12-17 13:39:15 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, answer = Integer.MIN_VALUE;
     static int[][] interview;
     static int[] dp;
-    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -15,25 +15,20 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         interview = new int[n][2];
         dp = new int[n+1];
-        visited = new boolean[n];
+        Arrays.fill(dp, -1);
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             interview[i][0] = Integer.parseInt(st.nextToken());
             interview[i][1] = Integer.parseInt(st.nextToken());
         }
-        dfs(0, 0);
-        System.out.println(answer);
+        dfs(0);
+        System.out.println(dp[0]);
     }
-    static int dfs(int idx, int price){
-        if (idx == n){
-            answer = Math.max(answer, price);
-            return 0;
-        }
-        if (idx > n-1) return 0;
-        visited[idx] = true;
-        dp[idx] = Math.max(dfs(idx+ interview[idx][0], price+ interview[idx][1]),
-                dfs(idx+1, price));
-        visited[idx] = false;
+    static int dfs(int idx){
+        if (idx == n) return 0;
+        if (idx > n-1) return Integer.MIN_VALUE;
+        if (dp[idx] != -1) return dp[idx];
+        dp[idx] = Math.max(dfs(idx+ interview[idx][0])+interview[idx][1], dfs(idx+1));
         return dp[idx];
     }
 }
