1e9d37b5525d999cca97239724e6d74085ba69a3
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2024-12-19 16:33:20 +0900

    [Gold V] Title: 실질적 약수, Time: 1012 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Gold/2247. 실질적 약수/실질적 약수.java
+++ 백준/Gold/2247. 실질적 약수/실질적 약수.java
@@ -5,12 +5,12 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         long n = Integer.parseInt(br.readLine());
-
         long sum = 0;
-        for (int i=2; i<n; i++)
+        for (int i = 2; i <= n/2; i++) {
             sum += ((n/i)-1) * i;
-
+        }
         System.out.println(sum%1000000);
     }
 }
