be04049a778f7c97f148a9c66c6c06264ef5359b
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2024-12-23 14:18:50 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 716 ms, Memory: 118356 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,32 +4,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
+    static int n, m, sum;
     static int[][] graph;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         graph = new int[n+1][n+1];
 
-        for (int i=1; i<=n; i++){
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=1; j<=n; j++){
+            for (int j = 1; j <= n; j++)
                 graph[i][j] = graph[i][j-1] + graph[i-1][j] - graph[i-1][j-1] + Integer.parseInt(st.nextToken());
-            }
         }
-        int answer = 0;
-        for (int i=0; i<m; i++){
+
+        sum = 0;
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            answer = graph[x2][y2] - graph[x1-1][y2] - graph[x2][y1-1]  + graph[x1-1][y1-1];
-            sb.append(answer).append("\n");
+            sum = graph[x2][y2] - graph[x1-1][y2] - graph[x2][y1-1] + graph[x1-1][y1-1];
+            sb.append(sum).append("\n");
         }
         System.out.println(sb);
     }
