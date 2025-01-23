989376fa9d461fb4b2b1fa51393a72de84ab68db
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-01-23 20:45:11 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,32 +1,31 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        // 1. 학생 배열 생성
-        int[] students = new int[n+2];
-        // reserve / lost 정보 반영
-        for(int l : lost)
-            students[l]--;
-        for(int r : reserve)
-            students[r]++;
         
-        // 여분 기준 앞뒤 확인해 체육복 빌려줌
-        for(int i=1; i<=n; i++){
-            if(students[i] == 1){
-                if(students[i-1] == -1){
-                    students[i]--;
-                    students[i-1]++;
-                }else if(students[i+1] == -1){
-                    students[i]--;
-                    students[i+1]++;
+        int[] student = new int[n+2];
+        
+        for(int i=0; i<lost.length; i++)
+            student[lost[i]]--;
+        
+        for(int i=0; i<reserve.length; i++)
+            student[reserve[i]]++;
+        
+        
+        for(int i=0; i<student.length; i++) {
+            if(student[i]==-1) {
+                if(student[i-1]==1) {
+                    student[i]++;
+                    student[i-1]--;
+                } else if(student[i+1]==1) {
+                    student[i]++;
+                    student[i+1]--;
                 }
             }
         }
         
-        // 체육복 갖고 있는 학생수 계산
-        for(int i=1; i<=n; i++){
-            if(students[i] >= 0) answer++;
-        }
         
+        for(int i=1; i<student.length-1; i++)
+            if(student[i]!=-1) answer++;
         return answer;
     }
 }
