bdb57749ce80806809e795426310ab8e81bf200c
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-01-27 23:41:03 +0900

    [level 0] Title: 코드 처리하기, Time: 348.86 ms, Memory: 374 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        char mode = '0';
+        for(int i=0; i<code.length(); i++){
+            if(code.charAt(i)=='1') {
+                if(mode=='1') mode='0';
+                else if(mode=='0') mode='1';
+            } else {
+                if(mode=='0' && code.charAt(i)!='1') {
+                    if(i%2==0) {
+                        answer += code.charAt(i);
+                    }
+                } else if(mode=='1') {
+                    if(i%2==1){
+                        answer += code.charAt(i);
+                    }
+                }
+            }
+        }
+        return "".equals(answer) ? "EMPTY" : answer;
+    }
+}
