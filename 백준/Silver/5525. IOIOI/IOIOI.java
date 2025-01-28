ad4f2bf7ac037497c8ab1c6b995712171f22d177
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-01-28 22:42:01 +0900

    [Silver I] Title: IOIOI, Time: 240 ms, Memory: 22040 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -9,24 +9,18 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
         char[] s = br.readLine().toCharArray();
-        String pn = pn(n);
 
-        int answer = 0;
-        for (int i = 0; i < m; i++) {
-            int cnt=0;
-            if (s[i]==pn.charAt(0)) {
+        int answer=0, cnt=0;
+        for (int i = 1; i < m-1; i++) {
+            if (s[i-1]=='I' && s[i]=='O' && s[i+1]=='I') {
                 cnt++;
-                for (int j = 1; j < pn.length(); j++) {
-                    if (i+j<s.length)
-                        if (s[i+j]==pn.charAt(j)) cnt++;
+                if (cnt==n) {
+                    cnt--;
+                    answer++;
                 }
-            }
-            if (cnt==pn.length()) answer++;
+                i++;
+            } else cnt=0;
         }
         System.out.println(answer);
-    }
-    static String pn(int n) {
-        String s = "I";
-        return s+"OI".repeat(n);
     }
 }
