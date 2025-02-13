1aabc7014d3ad4ba662ad42f04bc7c4d120c23ac
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-02-14 01:24:43 +0900

    [level 1] Title: 공원 산책, Time: 0.57 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        int[] answer = new int[2];
-        int y = -1,x = -1;
-        int yLen = park.length;
-        int xLen = park[0].length();
+        int[][] moves = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int y=-1, x=-1;
         
         for(int i=0; i<park.length; i++) {
             for(int j=0; j<park[i].length(); j++) {
@@ -19,78 +17,49 @@ class Solution {
             String[] s = route.split(" ");
             String direction = s[0];
             int dis = Integer.parseInt(s[1]);
-            int curY = y;
-            int curX = x;
-
-            switch (direction) {
-                case "E" -> {
-                    boolean flag = true;
-                    for (int j = 1; j <= dis; j++) {
-                        curX++;
-                        if (curX >= xLen) {
-                            flag = false;
-                            break;
-                        }
-                        if (park[curY].charAt(curX)=='X') {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag)
-                        x = curX;
-                }
-                case "S" -> {
-                    boolean flag = true;
-                    for (int j = 1; j <= dis; j++) {
-                        curY++;
-                        if (curY >= yLen) {
-                            flag = false;
-                            break;
-                        }
-                        if (park[curY].charAt(curX)=='X') {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag)
-                        y = curY;
-                }
-                case "W" -> {
-                    boolean flag = true;
-                    for (int j = 1; j <= dis; j++) {
-                        curX--;
-                        if (curX < 0) {
-                            flag = false;
-                            break;
-                        }
-                        if (park[curY].charAt(curX)=='X') {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag)
-                        x = curX;
-                }
-                case "N" -> {
-                    boolean flag = true;
-                    for (int j = 1; j <= dis; j++) {
-                        curY--;
-                        if (curY < 0) {
-                            flag = false;
-                            break;
-                        }
-                        if (park[curY].charAt(curX)=='X') {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag)
-                        y = curY;
-                }
+            
+            int index = getDirectionIndex(direction);
+            
+            if(isWalk(park, y, x, dis, moves[index])) {
+                y += dis*moves[index][1];
+                x += dis*moves[index][0];
+            }
+        
+        }
+        
+        return new int[]{y, x};
+    }
+    
+    static boolean isWalk(String[] park, int y, int x, int dis, int[] move) {
+        for(int i=0; i<dis; i++) {
+            y += move[1];
+            x += move[0];
+            if(0>y || y>=park.length || 0>x || x>=park[0].length() || park[y].charAt(x)=='X') return false;
+        }
+        
+        
+        return true;
+    }
+    
+    static int getDirectionIndex(String direction) {
+        int index = 0;
+        switch(direction) {
+            case "E" -> { 
+                break;
+            }
+            case "W" -> {
+                index = 1;
+                break;
+            }
+            case "S" -> {
+                index = 2;
+                break;
+            }
+            case "N" -> {
+                index = 3;
+                break;
             }
         }
-        answer[0] = y;
-        answer[1] = x;
-        return answer;
+        return index;
     }
 }
