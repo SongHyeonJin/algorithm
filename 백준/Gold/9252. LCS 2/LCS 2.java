ac5048b2f8819f311835b2b57742d6eca8117af7
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-02-16 22:30:39 +0900

    [Gold IV] Title: LCS 2, Time: 128 ms, Memory: 18316 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -11,34 +11,35 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         str1 = br.readLine().toCharArray();
-        str2 = br.readLine().toCharArray();
-        dp = new int[str1.length+1][str2.length+1];
+        str2 =  br.readLine().toCharArray();
+        int oneLen = str1.length;
+        int twoLen = str2.length;
+        dp = new int[oneLen+1][twoLen+1];
 
-        for (int i = 1; i <= str1.length; i++) {
-            for (int j = 1; j <= str2.length; j++) {
-                if (str1[i-1] == str2[j-1])
-                    dp[i][j] = dp[i-1][j-1]+1;
+        for (int i = 1; i <= oneLen; i++) {
+            for (int j = 1; j <= twoLen; j++) {
+                if (str1[i-1]==str2[j-1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 else
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        sb.append(dp[str1.length][str2.length]).append("\n");
-        lcs(str1, str1.length, str2.length);
+        sb.append(dp[oneLen][twoLen]).append("\n");
+        lcs(str1, oneLen, twoLen);
         System.out.println(sb);
     }
-
-    private static void lcs(char[] str, int i, int j) {
-        Stack<Character> s = new Stack<>();
-        while (i>0 && j>0){
-            if (dp[i][j] == dp[i-1][j]) i--;
-            else if (dp[i][j] == dp[i][j-1]) j--;
+    static void lcs(char[] str, int i, int j) {
+        Stack<Character> stack = new Stack<>();
+        while (i>0 && j>0) {
+            if (dp[i][j]==dp[i-1][j]) i--;
+            else if (dp[i][j]==dp[i][j-1]) j--;
             else {
-                s.push(str[i-1]);
+                stack.push(str[i-1]);
                 i--;
                 j--;
             }
         }
-        while (!s.isEmpty())
-            sb.append(s.pop());
+        while (!stack.isEmpty())
+            sb.append(stack.pop());
     }
 }
