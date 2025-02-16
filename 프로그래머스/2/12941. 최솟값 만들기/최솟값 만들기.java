df8b0e89d825e43cdb2f35549d8905799f059079
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-02-16 13:32:52 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.17 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -7,16 +7,11 @@ class Solution
         int answer = 0;
         int len = A.length;
 
-        int[] C = new int[len];
         Arrays.sort(A);
         Arrays.sort(B);
-        int idx=0;
-        for(int i=len-1; i>=0; i--) {
-            C[idx++] = B[i]; 
-        }
         
         for(int i=0; i<len; i++) {
-            answer += A[i]*C[i];
+            answer += A[i]*B[len-1-i];
         }
 
         return answer;
