d92578c3894de3193d399c6a71cf820f93c3d57d
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-02-19 16:16:20 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.85 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        Map<String, Integer> period = new HashMap<>();
+        int todayY = Integer.parseInt(today.substring(0, 4));
+        int todayM = Integer.parseInt(today.substring(5, 7));
+        int todayD = Integer.parseInt(today.substring(8, 10));
+        
+        for(int i=0; i<terms.length; i++) {
+            String[] s = terms[i].split(" ");
+            period.put(s[0], Integer.parseInt(s[1]));
+        }
+        
+        for(int i=0; i<privacies.length; i++) {
+            String[] s = privacies[i].split(" ");
+            int year = Integer.parseInt(s[0].substring(0, 4));
+            int month = Integer.parseInt(s[0].substring(5, 7));
+            int day = Integer.parseInt(s[0].substring(8, 10));
+            String type = s[1];
+            
+            int plusMonth = period.get(type);
+            
+            month += plusMonth;
+            while(month>12) {
+                year++;
+                month-=12;
+            }
+            day--;
+            if(day==0) {
+                month--;
+                if(month==3) {
+                    day+=28;
+                }
+                else if(month==5 || month==7 || month==10 || month==12) {
+                    day += 30;
+                }
+                else {
+                    day += 31;
+                }
+            }
+            
+            if(month==0) {
+                year--;
+                month+=12;
+            }
+            
+            if(todayY>year) {
+                answer.add(i+1);
+            } else if(todayY==year && todayM>month) {
+                answer.add(i+1);
+            } else if(todayY==year && todayM==month && todayD>day) {
+                answer.add(i+1);
+            }
+        }
+        return answer.stream().mapToInt(a -> a).toArray();
+    }
+}
