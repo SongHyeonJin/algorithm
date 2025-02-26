3f11bc5de791402df89c5d6533f2cb3650c1b8f7
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-02-26 20:17:31 +0900

    [level 3] Title: 단어 변환, Time: 0.32 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,28 +1,30 @@
 class Solution {
-    static boolean[] visited;
     static int answer = 0;
+    static boolean[] visited;
     public int solution(String begin, String target, String[] words) {
         visited = new boolean[words.length];
         dfs(begin, target, words, 0);
         return answer;
     }
-    static void dfs(String now, String target, String[] words, int cnt){
-        if(now.equals(target)){
+    static void dfs(String cur, String target, String[] words, int cnt) {
+        if(cur.equals(target)) {
             answer = cnt;
             return;
         }
-        for(int i=0; i<words.length; i++){
+        for(int i=0; i<words.length; i++) {
             if(visited[i]) continue;
-            int a = 0;
-            for(int j=0; j<now.length(); j++){
-               if(now.charAt(j) != words[i].charAt(j)) a++;
+            int dif = 0;
+            for(int j=0; j<cur.length(); j++) {
+                if(cur.charAt(j)!=words[i].charAt(j)) {
+                    dif++;
+                }
             }
-            if(a==1){
+            if(dif==1) {
                 visited[i] = true;
                 dfs(words[i], target, words, cnt+1);
                 visited[i] = false;
             }
+            
         }
     }
-    
 }
