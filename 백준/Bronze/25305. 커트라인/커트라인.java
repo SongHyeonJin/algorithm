2176388c7c3d39c7b4594f46eb82604c235f7eb3
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-03-24 22:11:47 +0900

    [Bronze II] Title: 커트라인, Time: 112 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int idx = 1, answer = 0;
+        for(int i=n-1; i>=0; i--) {
+            if (idx == k) {
+                answer = arr[i];
+                break;
+            }
+            idx++;
+        }
+        System.out.println(answer);
+    }
+}
