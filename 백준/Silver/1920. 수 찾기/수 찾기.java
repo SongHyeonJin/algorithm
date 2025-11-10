bd906526a90750ca3ee37796a1ad52d31cf758c7
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-11-10 18:33:55 +0900

    [Silver IV] Title: 수 찾기, Time: 1300 ms, Memory: 59104 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,35 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(nums);
-
-        int m = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<m; i++){
-            int answer = 0;
-            int start = 0;
-            int end = n-1;
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int answer = 0, start = 0, end = N - 1;
             int num = Integer.parseInt(st.nextToken());
-            while (start <= end){
-                int mid = (start+end) / 2;
-                if(nums[mid]==num){
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (A[mid] == num) {
                     answer = 1;
                     break;
                 }
-                if(nums[mid] > num) end  = mid - 1;
+
+                if (A[mid] > num) end = mid - 1;
                 else start = mid + 1;
             }
             System.out.println(answer);
