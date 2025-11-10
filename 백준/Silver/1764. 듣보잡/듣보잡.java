209020548b37c5ba6fbc2d2435df9b57e3dd6449
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-11-10 21:22:48 +0900

    [Silver IV] Title: 듣보잡, Time: 268 ms, Memory: 26424 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,28 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Set<String> people = new HashSet<>();
-        for(int i=0; i<N; i++){
-            people.add(br.readLine());
+        Set<String> noListen = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            noListen.add(br.readLine());
         }
 
         Set<String> dbj = new TreeSet<>();
-        for(int i=0; i<M; i++){
+        for (int i = 0; i < M; i++) {
             String person = br.readLine();
-            if(people.contains(person)) dbj.add(person);
+            if (noListen.contains(person)) dbj.add(person);
         }
 
-        System.out.println(dbj.size());
-        for(String person : dbj) System.out.println(person);
+        sb.append(dbj.size()).append("\n");
+        for (String person : dbj) sb.append(person).append("\n");
+
+        System.out.println(sb.toString());
     }
 }
