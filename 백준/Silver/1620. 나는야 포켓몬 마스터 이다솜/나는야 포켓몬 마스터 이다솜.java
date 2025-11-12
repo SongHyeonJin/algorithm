95aa31af7688ddf52de797e6734b561d40b7bc1f
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-11-12 22:24:38 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 460 ms, Memory: 57512 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -3,28 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String [] strs = br.readLine().split(" ");
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(strs[0]);
+		int M = Integer.parseInt(strs[1]);
+		
+		Map<Integer,String> map1 = new HashMap<>();
+		Map<String,Integer> map2 = new HashMap<>();
+		for(int i=1;i<=N;i++) {
+			String pocketmon = br.readLine();
+			map1.put(i, pocketmon);
+			map2.put(pocketmon, i);
+		}
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+		for(int i=0;i<M;i++) {
+			String s = br.readLine();
+			if(49 <= s.charAt(0) && s.charAt(0) <= 57) {
+				int key = Integer.parseInt(s);
+				sb.append(map1.get(key));
+			}
+			else {
+				
+				sb.append(map2.get(s));
+			}
+			
+			sb.append("\n");		
+		}
+		System.out.println(sb.toString());
+	}
 
-        Map<String, String> poketmon = new HashMap<>();
-        for(int i=1; i<=N; i++){
-            String name = br.readLine();
-            String num = Integer.toString(i);
-            poketmon.put(name, num);
-            poketmon.put(num, name);
-        }
-
-        for(int i=0; i<M; i++){
-            sb.append(poketmon.get(br.readLine())).append("\n");
-        }
-        System.out.println(sb);
-    }
 }
