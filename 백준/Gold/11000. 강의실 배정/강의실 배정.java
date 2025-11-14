9103bd00c326fff4868e245ccde9e1d521477bf6
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-11-14 13:19:03 +0900

    [Gold IV] Title: 강의실 배정, Time: 632 ms, Memory: 71868 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,40 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
         int n = Integer.parseInt(br.readLine());
-        int[][] time = new int[n][2];
-
-        for(int i=0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
-            time[i][0] = Integer.parseInt(st.nextToken());
-            time[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(time, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[0] == o2[0]) return o1[1] - o2[1];
+        StringTokenizer st;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> {
+            if(o1[0] == o2[0]){
+                return o1[1] - o2[1];
+            }else{
                 return o1[0] - o2[0];
             }
-        });
+        }));
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(time[0][1]);
-
-        for(int i=1; i<n; i++){
-            if(pq.peek() <= time[i][0]) pq.poll();
-            pq.add(time[i][1]);
+            pq.offer(new int[]{start, end});
         }
-        System.out.println(pq.size());
+        
+        PriorityQueue<Integer> room = new PriorityQueue<>();
+        room.offer(0);
+        
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            if(room.peek() <= cur[0]){
+                room.poll();
+            }
+            room.offer(cur[1]);
+        }
+
+        System.out.println(room.size());
     }
 }
