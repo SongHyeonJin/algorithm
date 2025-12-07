81a9ee9101af94f04b251cf7812da70f5a00ea8d
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-12-07 23:37:01 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 252 ms, Memory: 25744 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            if (s.equals("ENTER")) {
+                set.clear();
+            } else {
+                if (set.contains(s)) continue;
+                set.add(s);
+                num++;
+            }
+        }
+        
+        System.out.println(num);
+    }
+}
