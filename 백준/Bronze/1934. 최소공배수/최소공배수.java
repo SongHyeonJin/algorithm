9ce36550bb4cee99fb0f48c9064c3a454c4d6405
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2025-12-09 23:17:29 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i <  T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            
+            sb.append(A*B/findGCD(A, B)).append("\n");
+        }
+        
+        System.out.println(sb.toString());
+    }
+
+    static int findGCD(int A, int B){
+        while(B != 0){
+            int R = A%B;
+            A = B;
+            B = R;
+        }
+        return A;
+    }
+}
