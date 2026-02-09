e135b703396aedfdb62ad76a580a823c79997eea
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-02-09 20:49:57 +0900

    [Gold V] Title: 암호 만들기, Time: 104 ms, Memory: 14760 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int L, C;
+    static char[] code;
+    static char[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        code = new char[L];
+        arr = new char[C];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            arr[i] = st.nextToken().charAt(0);
+        }
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+        
+        System.out.println(sb.toString());
+    }
+
+    public static void dfs(int depth, int start) {
+        if (depth == L) {
+            if (isValid(code)) {
+                for (char c : code) {
+                    sb.append(c);
+                }
+                sb.append("\n");
+            }
+            return;
+        }
+        for (int i = start; i < C; i++) {
+            code[depth] = arr[i];
+            dfs(depth+1, i+1);
+        }
+    }
+
+    public static boolean isValid(char[] code) {
+        int c = 0;
+        int v = 0;
+        for (int i = 0; i < code.length; i++) {
+            if (code[i] == 'a' || code[i] == 'e' || code[i] == 'i' || code[i] == 'o' || code[i] == 'u') {
+                v++;
+            } else {
+                c++;
+            }
+        }
+        if (c >= 2 && v >= 1) {
+            return true;
+        } else {
+            return  false;
+        }
+    }
+}
