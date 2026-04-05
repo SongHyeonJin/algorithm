a7bcb4c3eb7b91a742bd91a9aa64fc0be8121a65
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-04-05 23:16:34 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 372 ms, Memory: 18160 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static boolean[] prime = new boolean[10001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        isPrime();
+
+        int a = Integer.parseInt(br.readLine());
+        for(int i = 0; i < a; i++) {
+            int num = Integer.parseInt(br.readLine());
+            int res1 = 0;
+            int res2 = 0;
+            int min = 10000, abs = 0;
+
+            for(int j = 2; j < num; j++) {
+                if(prime[j] || prime[num - j]) continue;
+
+                abs = Math.abs(num - j - j);
+                if(min>abs) {
+                    res1 = j;
+                    res2 = num - j;
+                    min = abs;
+                }
+
+            }
+            System.out.println(res1 + " " + res2);
+        }
+
+    }
+
+    static void isPrime() {
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i < Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+
+            for(int j = i * i; j < prime.length; j+=i) {
+                prime[j] = true;
+            }
+        }
+    }
+}
