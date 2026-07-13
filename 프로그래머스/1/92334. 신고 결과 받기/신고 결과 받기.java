de375aa3e0e91cdfd673bf2b526c959dfb790cd6
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-07-13 22:50:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 90.94 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-        Map<String, HashSet<String>> users = new HashMap();
-        Map<String, Integer> idx = new HashMap<>();
-        
-        for(int i=0; i<id_list.length; i++) {
-            users.put(id_list[i], new HashSet<>());
-            idx.put(id_list[i], i);
+        Map<String, Set<String>> reportedBy = new HashMap<>();
+        Map<String, Integer> countMap = new LinkedHashMap<>();
+
+        for (String id : id_list) {
+            reportedBy.put(id, new HashSet<>());
+            countMap.put(id, 0);
         }
-        
-        for(String s : report) {
-            String[] person = s.split(" ");
-            users.get(person[1]).add(person[0]);
+
+        for (String s : report) {
+            String[] split = s.split(" ");
+            reportedBy.get(split[1]).add(split[0]);
         }
-        
-        for(String person : id_list) {
-            HashSet<String> send = users.get(person);
-            if (send.size() >= k) {
-                for(String name : send) answer[idx.get(name)]++;
+
+        for (String id : id_list) {
+            Set<String> reporters = reportedBy.get(id);
+            if (reporters.size() >= k) {
+                for (String reporter : reporters) {
+                    countMap.put(reporter, countMap.get(reporter) + 1);
+                }
             }
         }
-        
-        return answer;
+
+        return countMap.values().stream().mapToInt(Integer::intValue).toArray();
     }
 }
