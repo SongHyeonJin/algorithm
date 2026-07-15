4b6087470df8daf566bc6a8a4e8120b33101e4de
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-07-15 20:08:25 +0900

    [level 2] Title: 타겟 넘버, Time: 3.41 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,14 @@
 class Solution {
-    static int answer;
     public int solution(int[] numbers, int target) {
-        answer = 0;
-        dfs(numbers, target, 0, 0);
-        return answer;
+        return dfs(numbers, target, 0, 0);
     }
-    static void dfs(int[]numbers, int target, int depth, int sum){
-        if(depth==numbers.length){
-            if(sum==target) answer++;
-        }else{
-            dfs(numbers, target, depth+1, sum+numbers[depth]);
-            dfs(numbers, target, depth+1, sum-numbers[depth]);
+    
+    int dfs(int[] numbers, int target, int depth, int sum) {
+        if (depth == numbers.length) {
+            return (sum == target) ? 1 : 0;
         }
+        
+        return dfs(numbers, target, depth + 1, sum + numbers[depth]) +
+               dfs(numbers, target, depth + 1, sum - numbers[depth]);
     }
 }
