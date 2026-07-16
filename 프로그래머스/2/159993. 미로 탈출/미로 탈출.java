788709386fd3e46a245bfde7c600d413bd248c88
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-07-16 20:11:52 +0900

    [level 2] Title: 미로 탈출, Time: 5.76 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -3,9 +3,11 @@ import java.util.*;
 class Solution {
     private static final int[] dy = {1, 0, -1, 0};
     private static final int[] dx = {0, 1, 0, -1};
+
     public int solution(String[] maps) {
         int n = maps.length;
         int m = maps[0].length();
+        
         int[] start = new int[2], lever = new int[2], exit = new int[2];
         char[][] graph = new char[n][m];
         
@@ -20,28 +22,35 @@ class Solution {
         
         int toLever = bfs(graph, start, lever, n, m);
         int toExit = bfs(graph, lever, exit, n, m);
-
+        
         if (toLever == -1 || toExit == -1) return -1;
         return toLever + toExit;
     }
     
     private int bfs(char[][] graph, int[] start, int[] end, int n, int m) {
         Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
         int[][] dist = new int[n][m];
-
+        
         q.offer(start);
-        dist[start[0]][start[1]] = 1;
-
+        visited[start[0]][start[1]] = true;
+        dist[start[0]][start[1]] = 0;
+        
         while (!q.isEmpty()) {
             int[] curr = q.poll();
-
-            if (curr[0] == end[0] && curr[1] == end[1]) return dist[curr[0]][curr[1]] - 1;
-
+            
+            if (curr[0] == end[0] && curr[1] == end[1]) {
+                return dist[curr[0]][curr[1]];
+            }
+            
             for (int i = 0; i < 4; i++) {
                 int ny = curr[0] + dy[i];
                 int nx = curr[1] + dx[i];
-
-                if (ny >= 0 && ny < n && nx >=0 && nx < m && graph[ny][nx] != 'X' && dist[ny][nx] == 0) {
+                
+                if (ny >= 0 && ny < n && nx >= 0 && nx < m 
+                    && graph[ny][nx] != 'X' && !visited[ny][nx]) {
+                    
+                    visited[ny][nx] = true;
                     dist[ny][nx] = dist[curr[0]][curr[1]] + 1;
                     q.offer(new int[]{ny, nx});
                 }
