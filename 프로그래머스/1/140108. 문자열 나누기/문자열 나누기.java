8839ed907971ab177f8d46bb1c2e558ccd1a0ad4
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-07-24 18:53:17 +0900

    [level 1] Title: 문자열 나누기, Time: 0.33 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,19 +1,24 @@
 class Solution {
     public int solution(String s) {
-        int answer = 1;
+        if (s == null || s.isEmpty()) return 0;
         
+        int answer = 0;
         char x = s.charAt(0);
-        int count = 1;
-        for(int i=1; i<s.length(); i++){
-            if(count==0){
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (count == 0) {
                 answer++;
                 x = s.charAt(i);
             }
-            if(x == s.charAt(i))
+            
+            if (x == s.charAt(i)) {
                 count++;
-            else
+            } else {
                 count--;
+            }
         }
+        
         return answer;
     }
 }
