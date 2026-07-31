eeae57a0deec1d6bc1073a2f016df0c78a326391
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-07-31 18:40:44 +0900

    [level 2] Title: 더 맵게, Time: 660.04 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,21 +1,24 @@
-import java.util.*;
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i : scoville)
-            pq.offer(i);
         
-        while(pq.size()>1){
-            int min = pq.poll();
-            if(min>=K) break;
-            int nxtMin = pq.poll();
-            int newScovile = min+(nxtMin*2);
+        for (int s : scoville)  pq.add(s);
+        
+        while (pq.peek() < K) {
+            if (pq.size() < 2) return -1;
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            int mixed = first + (second * 2);
+            pq.add(mixed);
+            
             answer++;
-            pq.offer(newScovile);
         }
         
-        return pq.poll()<K ? -1 : answer;
+        return answer;
     }
 }
