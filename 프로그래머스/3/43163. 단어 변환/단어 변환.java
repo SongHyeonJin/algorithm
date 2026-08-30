0d8d06eafe44ba5af05438a27561f5c0b7914489
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-08-30 18:15:31 +0900

    [level 3] Title: 단어 변환, Time: 0.66 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,30 +1,63 @@
+import java.util.*;
+
 class Solution {
-    static int answer = 0;
-    static boolean[] visited;
-    public int solution(String begin, String target, String[] words) {
-        visited = new boolean[words.length];
-        dfs(begin, target, words, 0);
-        return answer;
-    }
-    static void dfs(String cur, String target, String[] words, int cnt) {
-        if(cur.equals(target)) {
-            answer = cnt;
-            return;
+    static class Node {
+        String word;
+        int count;
+
+        public Node(String word, int count) {
+            this.word = word;
+            this.count = count;
         }
-        for(int i=0; i<words.length; i++) {
-            if(visited[i]) continue;
-            int dif = 0;
-            for(int j=0; j<cur.length(); j++) {
-                if(cur.charAt(j)!=words[i].charAt(j)) {
-                    dif++;
-                }
-            }
-            if(dif==1) {
-                visited[i] = true;
-                dfs(words[i], target, words, cnt+1);
-                visited[i] = false;
+    }
+
+    public int solution(String begin, String target, String[] words) {
+        if (!hasTarget(target, words)) {
+            return 0;
+        }
+        
+        return bfs(begin, target, words);
+    }
+    
+    private int bfs(String begin, String target, String[] words) {
+        Queue<Node> q = new LinkedList<>();
+        boolean[] visited = new boolean[words.length];
+        
+        q.add(new Node(begin, 0));
+        
+        while (!q.isEmpty()) {
+            Node current = q.poll();
+            
+            if (current.word.equals(target)) {
+                return current.count;
             }
             
+            for (int i = 0; i < words.length; i++) {
+                if (!visited[i] && isChangeable(current.word, words[i])) {
+                    visited[i] = true;
+                    q.add(new Node(words[i], current.count + 1));
+                }
+            }
         }
+        
+        return 0;
+    }
+    
+    private boolean hasTarget(String target, String[] words) {
+        for (String w : words) {
+            if (w.equals(target)) return true;
+        }
+        return false;
+    }
+
+    private boolean isChangeable(String w1, String w2) {
+        int diffCount = 0;
+        for (int i = 0; i < w1.length(); i++) {
+            if (w1.charAt(i) != w2.charAt(i)) {
+                diffCount++;
+            }
+            if (diffCount > 1) return false;
+        }
+        return diffCount == 1;
     }
 }
