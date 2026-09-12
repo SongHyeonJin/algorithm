46f7604b8f011d9f6bc798cbb5bb753c1fd96fe9
SongHyeonJin <101760007+SongHyeonJin@users.noreply.github.com>
2026-09-12 22:56:27 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,14 +1,13 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
-        int answer=0;
-        while(a != b) {
-            a = a/2 + a%2;
-            b = b/2 + b%2;
-            
-            answer++;
+class Solution {
+    public int solution(int n, int a, int b) {
+        int round = 0;
+        
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
         }
-        return answer;
+        
+        return round;
     }
 }
